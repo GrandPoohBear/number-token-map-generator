e7b5cf8d1a912ffd4d89bc9fdca18035557b5efc
Andy Isaacson <andy@tutukain.com>
2021-01-02 19:33:03 -0800

    Making into a more proper package

---
+++ src/write-token-map.ts
@@ -0,0 +1,13 @@
+import _ from 'lodash';
+import { buildTokenMap } from './token-map';
+import { getAllSubstrings } from './string-utility';
+import * as fs from 'fs';
+
+console.log('Building token map');
+const tokenMap = buildTokenMap();
+console.log(
+  `Histogram`,
+  _.countBy(Object.values(tokenMap).map((a) => a.length))
+);
+
+fs.writeFileSync('./output-token-map.json', JSON.stringify(tokenMap));
